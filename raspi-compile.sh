2a2db5ed80f9b5e9597c4cdf9bc98f39ee7f614e
isilvalun <isaac.silva@usach.cl>
2021-02-14 23:27:05 -0300

    script tweaks (2)

--- raspi-compile.sh
+++ raspi-compile.sh
@@ -8,7 +8,7 @@ export MAKEFLAGS="-j$(nproc) LOCALVERSION=-raspi"
 export DEBIAN_KERNEL_DISABLE_DEBUG=yes
 export KBUILD_DEBARCH=arm64
 export TARGET_LIST=arm64
-echo nice make "$MAKEFLAGS" ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bindeb-pkg
-nice make "$MAKEFLAGS" ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bindeb-pkg
+echo nice make $MAKEFLAGS ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bindeb-pkg
+nice make $MAKEFLAGS ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bindeb-pkg
 
 exit 0
