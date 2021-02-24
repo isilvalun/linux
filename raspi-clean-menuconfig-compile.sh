dbe462fcd940bca327a4bf39f3f66168bc6e05d0
isilvalun <isaac.silva@usach.cl>
2021-02-24 04:41:57 -0300

    Tweaking clean-menuconfig script

--- raspi-clean-menuconfig-compile.sh
+++ raspi-clean-menuconfig-compile.sh
@@ -5,12 +5,12 @@ export ARCH=arm64
 export FEATURESET=none
 export FLAVOUR=raspi
 export PATH=/usr/lib/ccache:"$PATH"
-export MAKEFLAGS="-j$(nproc) LOCALVERSION=-raspi"
+export MAKEFLAGS="-j$(nproc) LOCALVERSION=-raspi ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-"
 export DEBIAN_KERNEL_DISABLE_DEBUG=yes
 export KBUILD_DEBARCH=arm64
 export TARGET_LIST=arm64
-make clean && make mrproper && git reset --hard
-make ARCH=arm64 menuconfig
-nice make $MAKEFLAGS ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bindeb-pkg
+make $MAKEFLAGS clean && make $MAKEFLAGS mrproper && git reset --hard
+make $MAKEFLAGS menuconfig
+make $MAKEFLAGS bindeb-pkg
 
 exit 0
