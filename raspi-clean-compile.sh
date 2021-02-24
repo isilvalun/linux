bc6e6968827407f0bff4386eb38fff008e71ab38
isilvalun <isaac.silva@usach.cl>
2021-02-24 04:25:43 -0300

    Adding full clean compile script.

---
+++ raspi-clean-compile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -Rfv arch/arm/boot/dts/overlays/
+make clean && make mrproper && git reset --hard
+export ARCH=arm64
+export FEATURESET=none
+export FLAVOUR=raspi
+export PATH=/usr/lib/ccache:"$PATH"
+export MAKEFLAGS="-j$(nproc) LOCALVERSION=-raspi"
+export DEBIAN_KERNEL_DISABLE_DEBUG=yes
+export KBUILD_DEBARCH=arm64
+export TARGET_LIST=arm64
+echo nice make $MAKEFLAGS ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bindeb-pkg
+nice make $MAKEFLAGS ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bindeb-pkg
+
+exit 0
