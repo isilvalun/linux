94433ef0fbd0b774576957655ce2e890a183c0b2
isilvalun <isaac.silva@usach.cl>
2021-02-27 04:47:59 -0300

    Build scripts tweaks

--- raspi-clean-compile.sh
+++ raspi-clean-compile.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-export MAKEFLAGS="-j$(nproc) DEBIAN_KERNEL_DISABLE_DEBUG=yes KBUILD_DEBARCH=arm64 KBUILD_BUILD_VERSION=2 KERNELRELEASE=5.11.0-rc6-raspi TARGET_LIST=arm64 LOCALVERSION=-raspi ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu-"
+export MAKEFLAGS="-j$(nproc) DEBIAN_KERNEL_DISABLE_DEBUG=yes KBUILD_DEBARCH=arm64 KERNELRELEASE=5.11.0-rc6 TARGET_LIST=arm64 LOCALVERSION=-raspi ARCH=arm64 FLAVOUR=raspi FEATURESET=none CROSS_COMPILE=aarch64-linux-gnu-"
 rm -Rfv arch/arm/boot/dts/overlays/
 make $MAKEFLAGS clean && make $MAKEFLAGS mrproper && git reset --hard
-export ARCH=arm64
-export FEATURESET=none
-export FLAVOUR=raspi
 export PATH=/usr/lib/ccache:"$PATH"
-export DEBIAN_KERNEL_DISABLE_DEBUG=yes
-export KBUILD_DEBARCH=arm64
-export KBUILD_BUILD_VERSION=2
-export KERNELRELEASE=5.11.0-rc6-raspi
-export TARGET_LIST=arm64
 make $MAKEFLAGS bindeb-pkg
 
 exit 0
