193ec27008a4320a3a3fff59d2666d3d6c9ab20c
isilvalun <isaac.silva@usach.cl>
2021-02-27 05:51:53 -0300

    Small script tweak

--- raspi-clean-menuconfig-compile.sh
+++ raspi-clean-menuconfig-compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export MAKEFLAGS="-j$(nproc) DEBIAN_KERNEL_DISABLE_DEBUG=yes KBUILD_DEBARCH=arm64 KERNELRELEASE=5.11.0-rc6 TARGET_LIST=arm64 LOCALVERSION=-raspi ARCH=arm64 FLAVOUR=raspi FEATURESET=none CROSS_COMPILE=aarch64-linux-gnu-"
+export MAKEFLAGS="-j$(nproc) DEBIAN_KERNEL_DISABLE_DEBUG=yes KBUILD_DEBARCH=arm64 KERNELRELEASE=5.11.0-rc6 TARGET_LIST=arm64 ARCH=arm64 FLAVOUR=raspi FEATURESET=none CROSS_COMPILE=aarch64-linux-gnu-"
 rm -Rfv arch/arm/boot/dts/overlays/
 make $MAKEFLAGS clean && make $MAKEFLAGS mrproper && git reset --hard
 export PATH=/usr/lib/ccache:"$PATH"
